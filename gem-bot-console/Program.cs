0294f7a2cd930bbfe5d933f328511852ddee0fe0
Duong Nguyen Dai <duong.nguyen91@gmail.com>
2022-05-17 09:34:23 +0700

    initial

---
+++ gem-bot-console/Program.cs
@@ -0,0 +1,22 @@
+namespace gem_bot_console;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("=== PRESS `q` AT ANY TIME TO STOP THIS ===");
+
+        var bot = new GemBot();
+        bot.Start();
+
+        while (true)
+        {
+            var input = Console.ReadKey();
+
+            if (input.KeyChar == 'q')
+            {
+                break;
+            }
+        }
+    }
+}
